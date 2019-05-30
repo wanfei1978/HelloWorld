e5d0475b1cfb618ea0eeb82c2900e110cdf89650
wanfei1978 <49530295+wanfei1978@users.noreply.github.com>
2019-05-31 01:21:55 +0800

    Git Test!

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is a Test!");
+        System.out.println("This is a Git!");
     }
 }
