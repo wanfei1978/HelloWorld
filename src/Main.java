bbd7d4f687dd54ba56fe31087cdc5b39c5aa9f96
wanfei1978 <wanfei_dlut@163.com>
2026-08-15 15:48:17 +0800

    feat: add Hello,AI Viber Coding output

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("This is a Test!");
         System.out.println("This is a Git!");
         System.out.println("Hello,Workbuddy!");
+        System.out.println("Hello，AI Viber Coding");
     }
 }
